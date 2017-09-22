9d4debcafc4e8338b4dce0520bae25fe9653d32a
Simon Shields <simon@lineageos.org>
2017-09-22 15:49:06 +0200

    soong_config: Add TARGET_USES_NON_TREBLE_CAMERA variable
    * Soong product variable 'uses_non_treble_camera'
    
    Change-Id: I38c01769dfeb376a1de474e99f0d3e1faaa6b15f

--- build/soong/android/variable.go
+++ build/soong/android/variable.go
@@ -1,6 +1,11 @@
 package android
 type Product_variables struct {
+
+	Uses_non_treble_camera struct {
+		Cflags []string
+	}
 }
 
 type ProductVariables struct {
+	Uses_non_treble_camera  *bool `json:",omitempty"`
 }
--- build/soong/soong_config.mk
+++ build/soong/soong_config.mk
@@ -3,5 +3,6 @@ cardinal_soong:
 	$(hide) (\
 	echo '{'; \
 	echo '"Cardinal": {'; \
+	echo '    "Uses_non_treble_camera": $(if $(filter true,$(TARGET_USES_NON_TREBLE_CAMERA)),true,false)'; \
 	echo '},'; \
 	echo '') > $(SOONG_VARIABLES_TMP)
